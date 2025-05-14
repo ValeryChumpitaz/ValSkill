381e92def8c3f166e68e3d3bb43ecba1cf7a6c64
ValeryGiselleChumpitazCaycho <111779657+ValeryChumpitaz@users.noreply.github.com>
2025-05-14 22:38:32 +0000

    backend

---
+++ valskill-backend/src/main/java/pe/edu/valskill/valskill_backend/ValskillBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.valskill.valskill_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValskillBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValskillBackendApplication.class, args);
+	}
+
+}
---
+++ valskill-backend/src/test/java/pe/edu/valskill/valskill_backend/ValskillBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.valskill.valskill_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValskillBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
